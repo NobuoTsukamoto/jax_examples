6f8fc5040425513684a2f5a0413cdb67d519ea96
Nobuo Tsukamoto <nextremer.nb.o@gmail.com>
2024-07-22 19:27:51 +0900

    Update.

--- classification/configs/resnet_v1.py
+++ classification/configs/resnet_v1.py
@@ -20,24 +20,34 @@ def get_config():
     config.optimizer = "adamw"
     config.learning_rate = 0.004
     config.adamw_weight_decay = 0.05
-    config.warmup_epochs = 0.066
+    config.l2_weight_decay = 0.0
+    config.warmup_epochs = 20.0
     config.momentum = 0.9
-    config.batch_size = 64
+    config.batch_size = 128
     config.label_smoothing = 0.1
 
     config.num_epochs = 300
-    config.aug_type = "randaug"
-    config.random_erasing = True
-    config.mixup_and_cutmix = True
 
     # randomarug
+    config.aug_type = "randaug"
     config.randaug_num_layers = 2
     config.randaug_magnitude = 9
     config.randaug_cutout_const = 40.0
-    config.randaug_translate_const = 0.0
+    config.randaug_translate_const = 10
     config.randaug_magnitude_std = 0.5
     config.randaug_prob_to_apply = None
     config.randaug_exclude_ops = ["Cutout"]
+
+    # random erasing
+    config.random_erasing = False
+
+    # mixup and cutmix
+    config.mixup_and_cutmix = True
+    config.mixup_and_cutmix_mixup_alpha = 0.8
+    config.mixup_and_cutmix_cutmix_alpha = 1.0
+    config.mixup_and_cutmix_prob = 1.0
+    config.mixup_and_cutmix_switch_prob = 0.5
+    config.mixup_and_cutmix_label_smoothing = 0.1
 
     config.init_stochastic_depth_rate = 0.1
 
