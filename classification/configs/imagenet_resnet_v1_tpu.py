3b90f3c70c5fba62a78a99aa4281101dca97bcb6
Nobuo Tsukamoto <nextremer.nb.o@gmail.com>
2024-09-11 22:17:57 +0900

    Update.

--- classification/configs/imagenet_resnet_v1_tpu.py
+++ classification/configs/imagenet_resnet_v1_tpu.py
@@ -22,13 +22,14 @@ def get_config():
 
     # optimizer config
     config.optimizer = "sgd"
-    config.learning_rate = 0.1
+    config.learning_rate = 0.8
     config.warmup_epochs = 5.0
     config.momentum = 0.9
 
     config.cache = True
     config.half_precision = True
     config.batch_size = 2048
+    config.shuffle_buffer_size = 16 * 1024
 
     config.num_epochs = 100
 
