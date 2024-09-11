0944014e7e894f9c3b4dbb6811c1ffdc1521f145
Nobuo Tsukamoto <nextremer.nb.o@gmail.com>
2024-09-12 08:19:51 +0900

    Update

--- classification/configs/imagenet_resnet50_v1_training_techniques_tpu.py
+++ classification/configs/imagenet_resnet50_v1_training_techniques_tpu.py
@@ -22,11 +22,13 @@ def get_config():
 
     # optimizer config
     config.optimizer = "adamw"
-    config.learning_rate = 0.004
     config.weight_decay = 0.05
     config.l2_weight_decay = 0.0
+
+    # LR scheduler config
+    config.learning_rate = 0.004
+    config.end_learning_rate = 1e-6
     config.warmup_epochs = 20.0
-    config.momentum = 0.9
 
     config.cache = True
     config.half_precision = True
@@ -35,6 +37,7 @@ def get_config():
     config.label_smoothing = 0.1
     config.model_ema_decay = 0.9999
     config.model_ema = True
+    config.init_stochastic_depth_rate = 0.1
 
     config.num_epochs = 300
 
@@ -82,7 +85,5 @@ def get_config():
     config.mixup_and_cutmix_prob = 1.0
     config.mixup_and_cutmix_switch_prob = 0.5
     config.mixup_and_cutmix_label_smoothing = 0.1
-
-    config.init_stochastic_depth_rate = 0.1
 
     return config
