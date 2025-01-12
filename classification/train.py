d2fb772eaf0f758cf2581529767d6da67e9a3ac2
Nobuo Tsukamoto <nextremer.nb.o@gmail.com>
2025-01-12 16:23:51 +0900

    Refactor training and evaluation to integrate Exponential Moving Average (EMA) support

--- classification/train.py
+++ classification/train.py
@@ -101,7 +101,7 @@ def train_step(
     stochastic_depth_rng=None,
     with_batchnorm=True,
     gradient_accumulation_steps=1,
-    ema_decay=0.0,
+    model_ema=False,
 ):
     """Perform a single training step."""
 
@@ -179,20 +179,16 @@ def train_step(
         )
         metrics["scale"] = dynamic_scale.scale
 
-    if ema_decay > 0.0:
-        new_state = jax.lax.cond(
-            step % gradient_accumulation_steps == 0,
-            lambda _: new_state.replace(ema_params=new_state.apply_ema()),
-            lambda _: new_state,
-            None,
-        )
+    if state.ema_tx is not None and state.ema_state is not None:
+        _, new_ema_state = state.ema_tx.update(new_state.params, state.ema_state)
+        new_state = new_state.replace(ema_state=new_ema_state)
 
     return new_state, metrics, new_dropout_rng, new_stochastic_depth_rng
 
 
 def eval_step(state, batch, num_classes, with_batchnorm, model_ema=False):
     if model_ema:
-        params = state.ema_params
+        params = state.ema_state.ema
     else:
         params = state.params
 
@@ -263,6 +259,12 @@ def create_train_state(
     params, batch_stats = initialized(rngs, config.image_size, model)
     tx = create_optimizer(config, learning_rate_fn)
 
+    ema_tx = None
+    ema_state = None
+    if config.model_ema:
+        ema_tx = optax.ema(config.model_ema_decay)
+        ema_state = ema_tx.init(params)
+
     if batch_stats is not None:
         state = TrainStateWithBatchNorm.create(
             apply_fn=model.apply,
@@ -270,8 +272,8 @@ def create_train_state(
             tx=tx,
             batch_stats=batch_stats,
             dynamic_scale=dynamic_scale,
-            ema_params=params,
-            ema_decay=config.model_ema_decay,
+            ema_tx=ema_tx,
+            ema_state=ema_state,
         )
         with_batchnorm = True
     else:
@@ -281,8 +283,8 @@ def create_train_state(
             params=params,
             tx=tx,
             dynamic_scale=dynamic_scale,
-            ema_params=params,
-            ema_decay=config.model_ema_decay,
+            ema_tx=ema_tx,
+            ema_state=ema_state,
         )
         with_batchnorm = False
 
@@ -355,10 +357,9 @@ def train_and_evaluate(config: ml_collections.ConfigDict, workdir: str):
     )
 
     learning_rate_fn = create_learning_rate_fn(config, steps_per_epoch)
+    state, with_batchnorm = create_train_state(rngs, config, model, learning_rate_fn)
 
     checkpoint_manager = create_checkpoint_manager(workdir, config)
-
-    state, with_batchnorm = create_train_state(rngs, config, model, learning_rate_fn)
     state = restore_checkpoint(checkpoint_manager, state)
     # step_offset > 0 if restarting from checkpoint
     step_offset = int(state.step)
@@ -372,7 +373,7 @@ def train_and_evaluate(config: ml_collections.ConfigDict, workdir: str):
             label_smoothing=config.label_smoothing,
             with_batchnorm=with_batchnorm,
             gradient_accumulation_steps=config.gradient_accumulation_steps,
-            ema_decay=config.model_ema_decay,
+            model_ema=config.model_ema,
         ),
         axis_name="batch",
     )
