e58e9dab576a31ef434ff54417706b411e819ec2
Nobuo Tsukamoto <nextremer.nb.o@gmail.com>
2025-02-19 22:31:21 +0900

    Remove unnecessary whitespace in loss.py

--- classification/loss.py
+++ classification/loss.py
@@ -4,14 +4,12 @@
     See the LICENSE file in the project root for more information.
 """
 
-
 import jax.numpy as jnp
 import optax
 from flax.training import common_utils
+
 
 def cross_entropy_loss(logits, labels, num_classes):
     one_hot_labels = common_utils.onehot(labels, num_classes=num_classes)
     xentropy = optax.softmax_cross_entropy(logits=logits, labels=one_hot_labels)
     return jnp.mean(xentropy)
-
-
